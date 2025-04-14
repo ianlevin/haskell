a7653f50f6baa161bfb0d4f8668a6767041bd4d1
Yo <yo@uba.com.ar>
2025-04-14 13:56:21 -0300

    a

--- haskel.hs
+++ haskel.hs
@@ -1,29 +1,29 @@
-todoMenor :: (Float, Float) -> (Float, Float) -> Bool
-todoMenor (a, b) (c, d) = (a<c && b<d)
+parteEntera :: Float -> Integer
+parteEntera n | n < 1 = 0
+              | otherwise = 1 + parteEntera(n-1)
 
-type Punto2D = (Float, Float)
+cantDigitos :: Integer -> Integer
+cantDigitos n | n < 10 = 1
+              | otherwise = 1 + cantDigitos(div n 10)
 
-todoMenor2 :: Punto2D -> Punto2D -> Bool
-todoMenor2 a b = (fst a < fst b && snd a < snd b)
+iesimoDigito :: Integer -> Integer -> Integer
+iesimoDigito n i = mod (div n (10^(cantDigitos n - i))) 10
 
-posPrimerPar :: (Int, Int, Int) -> Int
-posPrimerPar (a, b, c) | mod a 2 == 0 = 1
-                       | mod b 2 == 0 = 2
-                       | mod c 2 == 0 = 3
-                       | otherwise = 4
+fibonacci :: Integer -> Integer
+fibonacci 0 = 1
+fibonacci 1 = 1
+fibonacci n = fibonacci (n-1) + fibonacci (n-2)
 
-bisiesto :: Int -> Bool
-bisiesto n = mod n 4 == 0 && (mod n 100 /= 0 || mod n 400 == 0)
+{-esDivisible :: Integer -> Integer -> Bool
+esDivisible x y | y > x = False
+                | y == x = True
+                | otherwise = esDivisible(x (2*y)) || esDivisible(x (3*y))-}
 
-type Tripla = (Float, Float, Float)
+esDivisible :: Integer -> Integer -> Bool              
+esDivisible x y | x == 0 = True
+                | x < 0 = False
+                | otherwise = esDivisible (x-y) y
 
-distanciaManhatan :: Tripla -> Tripla -> Float
-distanciaManhatan (a,b,c) (d,e,f) = abs (a-d) + abs (b-e) + abs (c-f)
-
-sumaUltimosDosDigitos :: Int -> Int
-sumaUltimosDosDigitos x = (mod (div (abs x) 10) 10 + mod (abs x) 10)
-
-comparar :: Int -> Int-> Int
-comparar a b | sumaUltimosDosDigitos a < sumaUltimosDosDigitos b = 1
-             | sumaUltimosDosDigitos a > sumaUltimosDosDigitos b = -1
-             |otherwise = 0
+esCapicua :: Integer -> Bool
+esCapicua n | cantDigitos n == 1 = True
+            | otherwise = div n (10^((cantDigitos n) -1)) == mod n 10 && esCapicua (mod (div n 10) (10^((cantDigitos(n)-2))))
