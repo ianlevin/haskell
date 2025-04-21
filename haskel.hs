1e74b70f07a7e47677b8024176df3d5384858d9b
The representation of blindness <yo@uba.com.ar>
2025-04-21 13:57:41 -0300

    recursividad 2 parametros

--- haskel.hs
+++ haskel.hs
@@ -27,3 +27,72 @@ esDivisible x y | x == 0 = True
 esCapicua :: Integer -> Bool
 esCapicua n | cantDigitos n == 1 = True
             | otherwise = div n (10^((cantDigitos n) -1)) == mod n 10 && esCapicua (mod (div n 10) (10^((cantDigitos(n)-2))))
+
+{-
+problema sumaPotencias(q:N, n:N, m:N): N{
+
+    requiere:{q > 0, n > 0, m > 0}
+
+    asegura:{resultado = }
+
+}
+
+
+-}
+sumatoriaInterna :: Integer -> Integer -> Integer -> Integer
+sumatoriaInterna _ _ 0 = 0
+sumatoriaInterna q i j = q^(i+j) + sumatoriaInterna q i (j-1)
+
+sumatoriaDoble :: Integer -> Integer -> Integer -> Integer
+sumatoriaDoble _ 0 _ = 0
+sumatoriaDoble q n m = sumatoriaInterna q n m + sumatoriaDoble q (n-1) m
+
+sumaPotencias :: Integer -> Integer -> Integer -> Integer
+sumaPotencias q n m = sumatoriaDoble q n m
+
+esDivisible :: Integer -> Integer -> Integer
+esDivisible n divisor | mod n divisor == 0 = divisor
+                      | otherwise = esDivisible n (divisor + 1) 
+
+menorDivisor :: Integer -> Integer
+menorDivisor n = esDivisible n 2 
+
+{-
+esPrimo :: Integer -> Bool
+esPrimo 
+
+nesimoPrimo :: Integer -> Integer
+nesimoPrimo n = 
+
+
+
+primoDivisor :: Integer -> Integer -> Integer
+primoDivisor n nprimo = mod n (nesimoPrimo nprimo) == 0 = (nesimoPrimo nprimo)
+
+menorDivisor :: Integer -> Integer
+menorDivisor n = primoDivisor n 1
+-}
+
+esPrimo :: Integer -> Bool
+
+nEsimoPrimoDesde :: Integer -> Integer -> Integer
+nEsimoPrimoDesde desde n 
+
+sumatoriaHastaNPrimosDesde :: Integer -> Integer -> Integer
+sumatoriaHastaNPrimosDesde n n = nEsimoPrimoDesde n
+sumatoriaHastaNPrimosDesde n desde = nEsimoPrimoDesde desde + sumatoriaHastaNPrimosDesde n (desde + 1)
+
+sumaInicialDePrimos :: Integer -> Integer -> Bool
+sumaInicialDePrimos n nprimo | (sumatoriaHastaNPrimosDesde nprimo 1) > n = False
+                             | n == (sumatoriaHastaNPrimosDesde nprimo 1) = True
+                             | otherwise = sumaInicialDePrimos n (nprimo + 1)
+
+
+
+esSumaInicialDePrimos :: Integer -> Bool
+esSumaInicialDePrimos n = sumaInicialDePrimos n 1
+
+
+
+
+
